6eca51ed1f41d99ee73eb0872e27de5d8cf93535
sofiyaOsadchi <sofiyahason@gmail.com>
2024-07-24 09:53:40 +0300

    orderi

--- src/services/order-service.ts
+++ src/services/order-service.ts
@@ -3,6 +3,46 @@ import Product from "../db/models/product-model";
 import { IOrderProduct } from "../@types/@types";
 
 export const orderService = {
+
+   /*  createOrder: async (userId: string, products: IOrderProduct[]) => {
+        try {
+            const orderProducts = await Promise.all(products.map(async product => {
+                const productDetails = await Product.findById(product.productId);
+                if (!productDetails) throw new Error("Product not found");
+                if (productDetails.quantity < product.quantity) throw new Error("Not enough stock");
+
+                // Update product stock
+                productDetails.quantity -= product.quantity;
+                productDetails.sold += product.quantity;
+                await productDetails.save();
+
+                return {
+                    productId: product.productId,
+                    title: productDetails.title,
+                    barcode: productDetails.barcode,
+                    quantity: product.quantity,
+                    price: productDetails.price,
+                    size: product.size, // שינוי זה לוקח את המידה מהבקשה
+                };
+            }));
+
+            // Calculate totalAmount
+            const totalAmount = orderProducts.reduce((acc, product) => acc + (product.quantity * product.price), 0);
+
+            const order = new Order({
+                userId,
+                products: orderProducts,
+                totalAmount,
+            });
+
+            return await order.save();
+        } catch (error) {
+            console.error("Error creating order:", error.message);
+            throw error;
+        }
+    },
+
+ */
     createOrder: async (userId: string, products: IOrderProduct[]) => {
         try {
             const orderProducts = await Promise.all(products.map(async product => {
@@ -21,7 +61,7 @@ export const orderService = {
                     barcode: productDetails.barcode,
                     quantity: product.quantity,
                     price: productDetails.price,
-                    size: productDetails.sizes,
+                    size: product.size,
                 };
             }));
 
